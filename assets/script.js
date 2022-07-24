9e59726b0b164d4aaf0adee79904261f1a57aa1e
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-07-23 21:51:41 -0700

    added another html page to serve as start and info screen

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+
+//function for the timer
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+//functiom to start the timer - need to change this to start with click, not with window.onload
+window.onload = function () {
+    var tenMinutes = 60 * 10,
+        display = document.querySelector('#time');
+    startTimer(tenMinutes, display);
+};
+
+// need to write a function that makes incorrect answer subtract 30 seconds from timer once I have all the variables made.

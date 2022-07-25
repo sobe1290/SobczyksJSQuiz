6422be3f93a565aca61f04a5c164015bf8e063ab
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-07-25 16:31:18 -0700

    added JS quiz shell, but need to debug

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-
 //function for the timer
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
@@ -17,7 +16,7 @@ function startTimer(duration, display) {
     }, 1000);
 }
 
-//function to start the timer
+//function to start the timer when quiz.html is loaded
 window.onload = function () {
     var tenMinutes = 60 * 10,
         display = document.querySelector('#time');
@@ -25,3 +24,158 @@ window.onload = function () {
 };
 
 // need to write a function that makes incorrect answer subtract 30 seconds from timer once I have all the variables made.
+
+function startQuiz(){
+    var output = [];
+    questionBank.forEach(
+        (currentQuestion, questionNumber) => {
+            var answers = [];
+            for(letter in currentQuestion.answers){
+                answers.push(
+                    `<label>
+                        <input type="radio" name="question${questionNumber}" value=${letter}">
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}                    
+                    </label>`
+                );
+            }
+            output.push(
+                `<div class="question"> ${currentQuestion.question} </div>
+                <div> class="answers"> ${answers.join('')} </div>`
+            );
+        }
+    );
+
+    quizElementContainer.innerHTML = output.join('');
+}
+
+function showResults(){
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+    let numCorrect = 0;
+    questionBank.forEach( (currentQuestion, questionNumber) => {
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        if(userAnswer === currentQuestion.correctAnswer){
+            numCorrect++;
+            answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        else {
+            answerContainers[questionNumber].style.color = 'red';
+        }
+    });
+
+    resultsContainer.innerHTML = `${numCorrect} out of ${questionBank.length}`;
+}
+
+var quizElementContainer = document.getElementById('quizElement');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+var questionBank = [
+    {
+        question: "Number 1 Question?",
+        answers: {
+            a: "Number 1 Answer 1",
+            b: "Number 1 Answer 2",
+            c: "Number 1 Answer 3",
+            d: "Number 1 Answer 4"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Number 2 Question?",
+        answers: {
+            a: "Number 2 Answer 1",
+            b: "Number 2 Answer 2",
+            c: "Number 2 Answer 3",
+            d: "Number 2 Answer 4"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Number 3 Question?",
+        answers: {
+            a: "Number 3 Answer 1",
+            b: "Number 3 Answer 2",
+            c: "Number 3 Answer 3",
+            d: "Number 3 Answer 4"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Number 4 Question?",
+        answers: {
+            a: "Number 4 Answer 1",
+            b: "Number 4 Answer 2",
+            c: "Number 4 Answer 3",
+            d: "Number 4 Answer 4"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Number 5 Question?",
+        answers: {
+            a: "Number 5 Answer 1",
+            b: "Number 5 Answer 2",
+            c: "Number 5 Answer 3",
+            d: "Number 5 Answer 4"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Number 6 Question?",
+        answers: {
+            a: "Number 6 Answer 1",
+            b: "Number 6 Answer 2",
+            c: "Number 6 Answer 3",
+            d: "Number 6 Answer 4"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Number 7 Question?",
+        answers: {
+            a: "Number 7 Answer 1",
+            b: "Number 7 Answer 2",
+            c: "Number 7 Answer 3",
+            d: "Number 7 Answer 4"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Number 8 Question?",
+        answers: {
+            a: "Number 8 Answer 1",
+            b: "Number 8 Answer 2",
+            c: "Number 8 Answer 3",
+            d: "Number 8 Answer 4"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Number 9 Question?",
+        answers: {
+            a: "Number 9 Answer 1",
+            b: "Number 9 Answer 2",
+            c: "Number 9 Answer 3",
+            d: "Number 9 Answer 4"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Number 10 Question?",
+        answers: {
+            a: "Number 10 Answer 1",
+            b: "Number 10 Answer 2",
+            c: "Number 10 Answer 3",
+            d: "Number 10 Answer 4"
+        },
+        correctAnswer: "c"
+    },
+];
+
+window.onload = startQuiz();
+
+submitButton.addEventListener('click', showResults);
+
